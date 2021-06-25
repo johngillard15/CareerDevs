3a1990df96bbcaa04d44b69217a156fdeef62cdb
johngillard15 <johngillard15@gmail.com>
2021-06-24 21:51:08 -0400

    Added docstring to Main.java

--- Java/Felicias_Fruit_Stand_3.0/src/com/company/Main.java
+++ Java/Felicias_Fruit_Stand_3.0/src/com/company/Main.java
@@ -4,9 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Felicia's Fruit Stand 3.0
+ * <h1>Felicia's Fruit Stand 3.0</h1>
+ * <p>Felicia's fruit stand has upgraded. She now serves fruits and meat now.</p>
+ *
+ * <p>Objects
+ *  <ol>1. Create a Product class this should contain data that any item she sells will have</ol>
+ *  <ol>2. Create a Fruit class that inherits from the Product class that contain fruit specific data.</ol>
+ *  <ol>3. Create a Meat class that inherits from the product class that contains Meat specific data.</ol>
+ * Be creative with your classes.</p>
+ * <br><br>
+ * <p>Code
+ *  <ol>1. In the main class create a Product list</ol>
+ *  <ol>2. add to the list three different Meats and 3 different Fruits.</ol>
+ *  <ol>3. in the main class create a method called displayProducts it should accept a Product list and display all the items in the list.</ol>
+ *  <ol>4. Create a second displayProducts method in the main class that accepts a Product list AND a string 'type' if the
+ *  type is meat only print the meat products and if fruit only display the fruit products</ol>
+ *  &emsp;&emsp;a. extra challenge try using a switch statement for this instead of ifelse
+ *  <ol>5. create a displayProduct method that accepts a product list and an int i. print out the product at index i.</p>
+ * <br><br>
+ * <p>be aware that except for the switch statement you should have all the code you need in Thursday's walkthrough.
+ * You'll need to adjust some items and use them in different ways to fit this exercise however there are new new concepts in this challenge.
+ * Try this by yourself first and see how far you get, You may work with a classmate starting on Tuesday.
+ * Of course if you get completely stuck you can ask for help in slack.</p>
+ *
+ * @since 6/20/2021
  * @author John Gillard
- * 6/20/2021
  */
 
 public class Main {

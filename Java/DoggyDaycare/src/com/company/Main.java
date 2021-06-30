9769f1f7bffa4748ecbd877d3dfd41afa3f3c2e7
johngillard15 <johngillard15@gmail.com>
2021-06-29 20:07:23 -0400

    fixed main to acccount for composition

--- Java/DoggyDaycare/src/com/company/Main.java
+++ Java/DoggyDaycare/src/com/company/Main.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * <h1>Doggy Daycare</h1>
  *
@@ -53,26 +50,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Animal> animalList = new ArrayList<>();
+        DayCare doggyDayCare = new DayCare();
 
         // Add animals to list
-        DayCare.addAnimal(animalList, new Animal(4, 1, false, "Frog", "Green"));
-        DayCare.addAnimal(animalList, new Dog(4, 2, true, "Fido", "Light Brown", "Golden Retriever", 3, false, "Yip!"));
-        DayCare.addAnimal(animalList, new Cat(4, 2, true, "Kitty", "White", "Black Spots", false));
-        DayCare.addAnimal(animalList, new Dog(4, 4, false, "Rufus", "Grey", "Tamaskan", 4, true, "Awoooooooo!"));
-        DayCare.addAnimal(animalList, new Cat(4, 3, false, "Maxwell", "Brown", "Black Stripes", true, true));
+        doggyDayCare.addAnimal(new Animal(4, 1, false, "Frog", "Green"));
+        doggyDayCare.addAnimal(new Dog(4, 2, true, "Fido", "Light Brown", "Golden Retriever", 3, false, "Yip!"));
+        doggyDayCare.addAnimal(new Cat(4, 2, true, "Kitty", "White", "Black Spots", false));
+        doggyDayCare.addAnimal(new Dog(4, 4, false, "Rufus", "Grey", "Tamaskan", 4, true, "Awoooooooo!"));
+        doggyDayCare.addAnimal(new Cat(4, 3, false, "Maxwell", "Brown", "Black Stripes", true, true));
 
         // Display all animals in list
-        DayCare.displayAnimals(animalList);
+        doggyDayCare.displayAnimals();
 
         // Remove an animal and display updated list
-        System.out.printf("\nRemove \"%s\"\n", animalList.get(0).name);
-        DayCare.removeAnimal(animalList, animalList.get(0).name);
-        DayCare.displayAnimals(animalList);
+        System.out.printf("\nRemove \"%s\"\n", doggyDayCare.animals.get(0).name);
+        doggyDayCare.removeAnimal(doggyDayCare.animals.get(0).name);
+        doggyDayCare.displayAnimals();
 
         // Let the animals loose!
         System.out.println("\nSpeak!");
-        for(Animal animal : animalList)
+        for(Animal animal : doggyDayCare.animals)
             animal.speak();
     }
 }
